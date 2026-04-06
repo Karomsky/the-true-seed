784d525919c5eb19e6f14241a277ab0c268d2b77
Romeo Alvarez <rova2k723@gmail.com>
2026-04-06 13:13:12 +0800

    feat: update One New Man study guide link to Google Drive

--- src/StudyPage.tsx
+++ src/StudyPage.tsx
@@ -69,7 +69,7 @@ export default function StudyPage({
     { id: 'true-church', name: t('The True Church', lang), icon: ShieldCheck, color: 'bg-brand-blue', pdfUrl: '/The_True_Church_Study_Guide.pdf', videoUrl: 'https://youtu.be/NrLZjwyCabU' },
     { id: 'eternal-covenant', name: t('Eternal Covenant', lang), icon: Infinity, color: 'bg-brand-blue', pdfUrl: '/Eternal_Covenant_Study_Guide.pdf', videoUrl: 'https://youtu.be/_42LiWGjFrk' },
     { id: 'election', name: t('Election', lang), icon: Star, color: 'bg-brand-gold', pdfUrl: '/Election_Study_Guide.pdf', videoUrl: 'https://youtu.be/JBq2SKDtSmk' },
-    { id: 'one-new-man', name: t('One New Man', lang), icon: Presentation, color: 'bg-brand-blue', pdfUrl: '/One_New_Man_Study_Guide.pdf', videoUrl: 'https://youtu.be/7g3BRtlJHj0?si=MPzIY__sTAGnJAMn' },
+    { id: 'one-new-man', name: t('One New Man', lang), icon: Presentation, color: 'bg-brand-blue', pdfUrl: 'https://drive.google.com/file/d/1guh3f-Ys164VYTsOgE3g89qWMBD_3zcF/preview?usp=drive_link', videoUrl: 'https://youtu.be/7g3BRtlJHj0?si=MPzIY__sTAGnJAMn' },
     { id: 'bookmarks', name: t('Bookmarks', lang), icon: Bookmark, color: 'bg-brand-dark' },
   ];
 
